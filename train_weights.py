0f1ccebdfcc629fae71873849bb323135ec2644e
Leonardo Alves <leonardo.af7@gmail.com>
2021-12-06 15:17:25 -0300

    Altera dimensão do input

--- train_weights.py
+++ train_weights.py
@@ -52,9 +52,14 @@ img_names=list(filter(r.match,img_names))
 print(f"{len(img_names)} images were found.")
 
 # Use of GPU
-torch.backends.cudnn.enabled = True #False #
-torch.backends.cudnn.benchmark = True #False #
-dtype = torch.cuda.FloatTensor #torch.FloatTensor #
+if torch.cuda.is_available():
+  torch.backends.cudnn.enabled = True #False #
+  torch.backends.cudnn.benchmark = True #False #
+  dtype = torch.cuda.FloatTensor #torch.FloatTensor #
+else:
+  torch.backends.cudnn.enabled = False
+  torch.backends.cudnn.benchmark = False
+  dtype = torch.FloatTensor
 
 # Radius of PSF with respect to deblur levels
 r_list=[1,2,3,4,6,8,9,11,13,15,17,18,20,21,22,26,31,35.5,41,44]
@@ -125,10 +130,10 @@ for i in range(arr_x.shape[0]-5,arr_x.shape[0]):
   valid_x.append(feature_extraction.image.extract_patches_2d(arr_x[i],patch_size=(patch_size, patch_size),max_patches=patch_p_img,random_state=i_rand))
   valid_y.append(feature_extraction.image.extract_patches_2d(arr_y[i],patch_size=(patch_size, patch_size),max_patches=patch_p_img,random_state=i_rand))
 
-train_x=np.concatenate(train_x,axis=0)
-train_y=np.concatenate(train_y,axis=0)
-valid_x=np.concatenate(valid_x,axis=0)
-valid_y=np.concatenate(valid_y,axis=0)
+train_x=np.expand_dims(np.concatenate(train_x,axis=0),1)
+train_y=np.expand_dims(np.concatenate(train_y,axis=0),1)
+valid_x=np.expand_dims(np.concatenate(valid_x,axis=0),1)
+valid_y=np.expand_dims(np.concatenate(valid_y,axis=0),1)
 
 del arr_x
 del arr_y_orig
@@ -205,8 +210,8 @@ elif(args.net_type=='skip'):
       # zero the gradients after updating
       optimizer.zero_grad()
 
-    if epoch % 10 == 0:
-      #[w, b] = deblur_net.parameters() # unpack parameters
-      print('epoch ', epoch+1, ' loss = ', l)
+      if i % 1 == 0:
+        #[w, b] = deblur_net.parameters() # unpack parameters
+        print('epoch ', epoch,' batch ', i, ' loss = ', l)
 else:
   pass
