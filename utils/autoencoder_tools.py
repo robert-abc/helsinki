9a23e6405dd1e89e92a82f49aa1f2a2dedf06a48
Leonardo Alves <leonardo.af7@gmail.com>
2021-12-12 18:05:02 -0300

    Add autoencoder architecture using pytorch and a function to train networks

--- utils/autoencoder_tools.py
+++ utils/autoencoder_tools.py
@@ -4,6 +4,125 @@ from tensorflow.keras.layers import Conv2D, MaxPool2D, UpSampling2D, Add
 import numpy as np
 import cv2
 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+# from pytorch_model_summary import summary
+# print(summary(Autoencoder(), torch.zeros((1, 1, 64, 64)), show_input=False))
+
+class Autoencoder(nn.Module):
+  def __init__(self, input_channel=1, pad_m='replicate'):
+    super(Autoencoder, self).__init__()
+    self.c1 = nn.Conv2d(input_channel, 64, 3, padding='same', padding_mode=pad_m)
+    self.c2 = nn.Conv2d(64, 64, 3, padding='same', padding_mode=pad_m)
+    
+    self.c3 = nn.Conv2d(64, 128, 3, padding='same', padding_mode=pad_m)
+    self.c4 = nn.Conv2d(128, 128, 3, padding='same', padding_mode=pad_m)
+
+    self.encoded = nn.Conv2d(128,256,3, padding='same', padding_mode=pad_m)
+
+    self.c5 = nn.Conv2d(256, 128, 3, padding='same', padding_mode=pad_m)
+    self.c6 = nn.Conv2d(128, 128, 3, padding='same', padding_mode=pad_m)
+    
+    self.c7 = nn.Conv2d(128, 64, 3, padding='same', padding_mode=pad_m)
+    self.c8 = nn.Conv2d(64, 64, 3, padding='same', padding_mode=pad_m)
+
+    self.decoded = nn.Conv2d(64, input_channel, 3, padding='same', padding_mode=pad_m)
+
+    self.pool = nn.MaxPool2d(2)
+    self.up = nn.Upsample(scale_factor=2)
+    
+  def forward(self,x):
+    x1 = F.relu(self.c1(x))
+    x2 = F.relu(self.c2(x1))
+    x3 = self.pool(x2)
+
+    x4 = F.relu(self.c3(x3))
+    x5 = F.relu(self.c4(x4))
+    x6 = self.pool(x5)
+
+    enc = F.relu(self.encoded(x6))
+
+    x7 = self.up(enc)
+    x8 = F.relu(self.c5(x7))
+    x9 = F.relu(self.c6(x8))
+    x10 = x5+x9
+
+    x11 = self.up(x10)
+    x12 = F.relu(self.c7(x11))
+    x13 = F.relu(self.c8(x12))
+    x14 = x13 + x2
+
+    dec = F.relu(self.decoded(x14))
+
+    return dec
+
+def train_model(model, train_loader, valid_loader,
+  loss=torch.nn.MSELoss(), optim='Adam', n_epoch=20,
+  val_loss_best=np.inf, loss_tol = 0, epoch_tol = -1,
+  l1_reg=0):
+
+  if(optim=='Adam'):
+    optimizer = torch.optim.Adam(model.parameters())
+
+  i_tol=0
+
+  for epoch in range(n_epoch):
+    model.train()
+    for i, (x, y) in enumerate(train_loader):
+      y_predicted = model(x)
+
+      # loss
+      l = loss(y, y_predicted)
+
+      if(l1_reg > 0):
+        param_layers = [lp.view(-1) for lp in model.parameters()]
+        param_w = torch.cat([param_layers[wi] for wi in range(0,len(param_layers),2)])
+        l1_loss = l1_reg * torch.norm(param_w, 1)
+        l = l + l1_loss
+
+      # calculate gradients = backward pass
+      l.backward()
+
+      # update weights
+      optimizer.step()
+
+      # zero the gradients after updating
+      optimizer.zero_grad()
+
+      if i % 10 == 0:
+        #[w, b] = deblur_net.parameters() # unpack parameters
+        print('epoch ', epoch,' batch ', i, ' loss = ', l.item())
+      
+    model.eval()
+    val_loss_batchs = []
+    with torch.no_grad():
+      for i, (x, y) in enumerate(valid_loader):
+        y_predicted = model(x)
+
+        # loss
+        l = loss(y, y_predicted)
+        val_loss_batchs.append(l.item())
+
+      val_loss_mean = np.array(val_loss_batchs).mean()
+      print('epoch ', epoch, ' val_loss = ', val_loss_mean)
+
+      if ((val_loss_mean+loss_tol) < (val_loss_best)):
+        val_loss_best = val_loss_mean
+        best_params = model.state_dict()
+        i_tol = 0
+      else:
+        i_tol+=1
+    
+    if(i_tol == epoch_tol):
+      print("Early Stop!")
+      break
+  
+  return best_params
+
+# def get_nn_pt(weight_path=None):
+
+
 def get_nn(weight_path=None):
     Input_img = Input(shape=(64,64,1))
 
